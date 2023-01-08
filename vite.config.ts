2e658199368f78d529afe3a4b75b383455c31693
WangZhenghai <15839366985@163.com>
2023-01-08 23:26:55 +0800

    新增：css解释器

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { join, resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import lightningcss from "vite-plugin-lightningcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), lightningcss({})],
   resolve: {
     // 配置路径别名
     alias: {
